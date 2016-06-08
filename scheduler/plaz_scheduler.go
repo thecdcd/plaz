98bf0fe746c30e6ce6bce260c3ac53b0c1ccdcd2
cdcd <cdcd@box1a.com>
2016-06-08 21:37:39 +0900

    Remove Scheduler Utils
    Move the scheduler utils into the scheduler file.

--- scheduler/plaz_scheduler.go
+++ scheduler/plaz_scheduler.go
@@ -4,6 +4,7 @@ import (
 	log "github.com/golang/glog"
 	mesos "github.com/mesos/mesos-go/mesosproto"
 	sched "github.com/mesos/mesos-go/scheduler"
+	util "github.com/mesos/mesos-go/mesosutil"
 	. "github.com/thecdcd/plaz/datalayer"
 )
 
@@ -103,4 +104,64 @@ func addSinglePointBatch(client DataDriver, name string, tags DataTags, fields D
 	} else {
 		log.Warningln("failed to create batch point: ", err)
 	}
+}
+
+func getOfferScalar(offer *mesos.Offer, name string) float64 {
+	resources := util.FilterResources(offer.Resources, func(res *mesos.Resource) bool {
+		return res.GetName() == name
+	})
+
+	value := 0.0
+	for _, res := range resources {
+		value += res.GetScalar().GetValue()
+	}
+
+	return value
+}
+
+func getOfferCpu(offer *mesos.Offer) float64 {
+	return getOfferScalar(offer, "cpus")
+}
+
+func getOfferMem(offer *mesos.Offer) float64 {
+	return getOfferScalar(offer, "mem")
+}
+
+func getOfferDisk(offer *mesos.Offer) float64 {
+	return getOfferScalar(offer, "disk")
+}
+
+func logOffers(offers []*mesos.Offer) {
+	for _, offer := range offers {
+		log.Infof("Received Offer <%v> with cpues=%v mem=%v", offer.Id.GetValue(), getOfferCpu(offer), getOfferMem(offer))
+	}
+}
+
+func offerToPoint(offer *mesos.Offer) (DataTags, DataFields) {
+	tags := DataTags{
+		"slave": offer.SlaveId.GetValue(),
+		"hostname": *offer.Hostname,
+	}
+	fields := DataFields{
+		"cpus": getOfferCpu(offer),
+		"mem": getOfferMem(offer),
+		"disk": getOfferDisk(offer),
+	}
+	return tags, fields
+}
+
+func execLostToPoint(exId *mesos.ExecutorID, slvId *mesos.SlaveID, exitCode int) (DataTags, DataFields) {
+	tags := DataTags{"slave": slvId.GetValue()}
+	fields := DataFields{
+		"executor": exId.GetValue(),
+		"exitcode": exitCode,
+	}
+	return tags, fields
+}
+
+func slaveLostToPoint(slvId *mesos.SlaveID) (DataTags, DataFields) {
+	fields := DataFields{
+		"slave": slvId.GetValue(),
+	}
+	return nil, fields
 }
